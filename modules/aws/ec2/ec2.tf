4448b05f2e7e1e511b7453e865fa260f9ed119e8
Shubham1996-aws <69294193+Shubham1996-aws@users.noreply.github.com>
2022-12-12 13:01:43 +0530

    Update ec2.tf

--- modules/aws/ec2/ec2.tf
+++ modules/aws/ec2/ec2.tf
@@ -42,19 +42,19 @@ resource "aws_eip_association" "eip_assoc" {
   rsa_bits = 4096
 }*/
 
-resource "aws_key_pair" "ssh" {
+/*resource "aws_key_pair" "ssh" {
   key_name = "devops-cml"
   #public_key = tls_private_key.ssh-key.public_key_openssh
   public_key = file("/ssh_key/id_rsa.pub")
   #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCuS7MeGC4iehHI4kLBR+HP17d+yLXbNs/LwgFMwtYZPE5LNNhPCO5P6bvsbTlEwT/iOBbDow6vSYhO1Y1VIquwWl1nsuq1mzAqPyIcgw20Ht748vYUOKdThDvftLCnnQ7e1dTsJuFbV2nZWNr4xPygbqTom57NooM1Nz0NzkgdH0d6Bj2Pk77T6He5PkUOU7Tc8n6ESdgRt+k+CFXJneTr9CNhfnyn80cpZGbQ/mi5pKgJPlUHtHJmd6epuhiTwfWtIU+/CZSUKVNKpobKTXWZfqUI62+dPlpkzOoMOET965yCKXK+fC91E23gZ3yE2OpsZztwd63nDHdKin9qLjlLbZdxMFnVlkuJtS6sR7kOABKJyFsqOQ/ipuZhEBr7Ybk/xTMExDfGp27n5SY2dhJNlU6sdRNTr7iVq4GORWAsu+1nS2Gqq0cjDlNId9QsGRjnUh9dzHwdRCiuT56GdY+Ho6zkcRM4taSlkyunA+pgzH8kFkSyoH1VDcdO9zUsaWNZG8W1YpRqdFv8yl1JmKJ9gKVPi750gFnWVvtc2jEtmMt8Mgn/lIIX7Wtu51c5zC+zKnspEvecAoVM9FF4VbnC3g8pKdgE4kwVaLbbX6w4yCEy1rVw1Q0OPlsyMQDNvGV5b5OKF+3eoVdpXNrbL7iEAqLtEgS1gfx+ZJOUhvJRfQ== cml-on-aws"
-}
+}*/
 ###################################################
 ########### Instance Creation ###########
 ###################################################
 resource "aws_instance" "web-server" {
   count                       = var.instanceCount
   ami                         = var.ami_id
-  key_name                    = aws_key_pair.ssh.key_name
+  key_name                    = var.key_name
   instance_type               = var.instance_type
   associate_public_ip_address = true
   subnet_id                   = var.subnet_id
